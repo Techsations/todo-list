4aafb71272c2c4ebee6be84f4525e8bf7369d12c
Techsations <akandedamilare.t@gmail.com>
2023-04-05 12:34:59 +0100

    I finished the delete button function

--- script.js
+++ script.js
@@ -5,16 +5,38 @@ let showErr = document.getElementById("showErr")
 let showTodo = document.getElementById("showTodo")
 
 
-function disArr() {
+function dispArr() {
     showTodo.innerHTML = "";
     todo.forEach((el, index) => {
         showTodo.innerHTML +=`
-        <div>
-          <h5>          
+        <div id="display">
+          <h5>${index} ${el} </h5>
+            <div class="btn-div">
+                <button id="deleteBtn" onclick='edit(${index})'>Edit</button>         
+                <button id="deleteBtn"  onclick='del(${index})'>Delete</button>
+            </div>
+        </div>         
         `
     })
 }
+    dispArr();
 
+    function addItems() {
+        if (todoItem.value == "") {
+            showErr.innerHTML = "Input field cannot be empty";
+        }
+        else {
+            showErr.innerHTML = "Todo successfully added";
+            todo.push(todoItem.value);
+            dispArr()
+        }
+    }
+
+function del(index) {
+    todo.splice(index,1);
+    dispArr()
+    
+}
 
 // function addItems() {
 //     if (input.value === "") {
@@ -44,37 +66,37 @@ function disArr() {
 //     }
 // }
 
-function del(rem) {
-    arrayTodo.splice(rem, 1);
-    showTodo.innerHTML = "";
-    arrayTodo.map((sh, rem) => {
-        console.log(sh, rem);
-        showTodo.innerHTML += `
-    <div class="display">
-        <h1>${sh}</h1>
-        <div>
-            <button id='delete-btn' onclick='edit(${rem})'>Edit</button>
-            <button id='delete-btn' onclick='del(${rem})'>Delete</button>
-          </div>
-    </div>
-    `
-    })
+// function del(rem) {
+//     arrayTodo.splice(rem, 1);
+//     showTodo.innerHTML = "";
+//     arrayTodo.map((sh, rem) => {
+//         console.log(sh, rem);
+//         showTodo.innerHTML += `
+//     <div class="display">
+//         <h1>${sh}</h1>
+//         <div>
+//             <button id='delete-btn' onclick='edit(${rem})'>Edit</button>
+//             <button id='delete-btn' onclick='del(${rem})'>Delete</button>
+//           </div>
+//     </div>
+//     `
+//     })
     
-}
+// }
 
-function edit(rem) {
-    arrayTodo.splice(rem, 1, input.value);
-    showTodo.innerHTML = "";
-    arrayTodo.map((sh, rem) => {
-        console.log(sh, rem);
-        showTodo.innerHTML += `
-    <div class="display">
-        <h1>${sh}</h1>
-        <div>
-            <button id='delete-btn' onclick='edit(${rem})'>Edit</button>
-            <button id='delete-btn' onclick='del(${rem})'>Delete</button>
-          </div>
-    </div>
-    `
-    })
-}
+// function edit(rem) {
+//     arrayTodo.splice(rem, 1, input.value);
+//     showTodo.innerHTML = "";
+//     arrayTodo.map((sh, rem) => {
+//         console.log(sh, rem);
+//         showTodo.innerHTML += `
+//     <div class="display">
+//         <h1>${sh}</h1>
+//         <div>
+//             <button id='delete-btn' onclick='edit(${rem})'>Edit</button>
+//             <button id='delete-btn' onclick='del(${rem})'>Delete</button>
+//           </div>
+//     </div>
+//     `
+//     })
+// }
