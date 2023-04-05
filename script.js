0e560f49f05da82c2c2093d6658a680e80f40e84
Techsations <akandedamilare.t@gmail.com>
2023-04-05 10:53:02 +0100

    I am Creating a Todo List

---
+++ script.js
@@ -0,0 +1,80 @@
+let todo = [];
+let addBtn = document.getElementById("addBtn");
+let todoItem = document.getElementById("todoItem");
+let showErr = document.getElementById("showErr")
+let showTodo = document.getElementById("showTodo")
+
+
+function disArr() {
+    showTodo.innerHTML = "";
+    todo.forEach((el, index) => {
+        showTodo.innerHTML +=`
+        <div>
+          <h5>          
+        `
+    })
+}
+
+
+// function addItems() {
+//     if (input.value === "") {
+//         alert("Input Something");
+//         return;
+//     }
+//     else {
+//         if (arrayTodo.includes(input.value)) {
+//             alert("Items already exist");
+//             return;
+//         }
+//         else {
+//             showTodo.innerHTML = "";
+//             arrayTodo.push(input.value);
+//             arrayTodo.map((arrayElement, index) => {
+//                 showTodo.innerHTML += `
+//             <div class="display">
+//                 <h1 id="arrayItem">${arrayElement}</h1>
+//                 <div>
+//                     <button id='delete-btn' onclick='edit(${index})'>Edit</button>
+//                     <button id='delete-btn' onclick='del(${index})'>Delete</button>
+//                  </div>
+//             </div>
+//             `
+//             })
+//         }
+//     }
+// }
+
+function del(rem) {
+    arrayTodo.splice(rem, 1);
+    showTodo.innerHTML = "";
+    arrayTodo.map((sh, rem) => {
+        console.log(sh, rem);
+        showTodo.innerHTML += `
+    <div class="display">
+        <h1>${sh}</h1>
+        <div>
+            <button id='delete-btn' onclick='edit(${rem})'>Edit</button>
+            <button id='delete-btn' onclick='del(${rem})'>Delete</button>
+          </div>
+    </div>
+    `
+    })
+    
+}
+
+function edit(rem) {
+    arrayTodo.splice(rem, 1, input.value);
+    showTodo.innerHTML = "";
+    arrayTodo.map((sh, rem) => {
+        console.log(sh, rem);
+        showTodo.innerHTML += `
+    <div class="display">
+        <h1>${sh}</h1>
+        <div>
+            <button id='delete-btn' onclick='edit(${rem})'>Edit</button>
+            <button id='delete-btn' onclick='del(${rem})'>Delete</button>
+          </div>
+    </div>
+    `
+    })
+}
